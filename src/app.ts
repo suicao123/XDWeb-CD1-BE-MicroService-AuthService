28b79166d624267e447e6b8a91a8ed25f7a589f3
mnhquan <mnhquan0109@gmail.com>
2026-02-05 22:25:03 +0700

    video 33

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,14 @@ import 'dotenv/config'
 
 const app=express()
 const PORT=process.env.PORT || 8080;
+
+//config view engine
+app.set('view engine', 'ejs');
+app.set('views',__dirname+'/views');
+
 app.get("/",(req,res)=>{
   
-    res.send("Hello World update !!!") 
+   res.render("home.ejs")
 })
 app.get("/test",(req,res)=>{
   
