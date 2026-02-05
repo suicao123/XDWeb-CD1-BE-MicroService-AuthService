c26f7448c061ab5296fdf8d67f2bcb5349ca98c0
mnhquan <mnhquan0109@gmail.com>
2026-02-05 18:55:25 +0700

    Video 24

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const express=require('express')
+
+const app=express()
+const PORT=8080;
+app.get("/",(req,res)=>{
+  
+    res.send("Hello World") 
+})
+app.get("/test",(req,res)=>{
+  
+    res.send("test routing") 
+})
+
+app.listen(PORT,()=>{
+    console.log(`My app is running on port:${PORT}`)
+})
