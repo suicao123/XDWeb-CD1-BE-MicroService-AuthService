a760546e487f8d7dab7685df868c599df3c1420d
mnhquan <mnhquan0109@gmail.com>
2026-02-15 18:43:35 +0700

    Video 76

---
+++ src/controllers/admin/dashboard.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+const getDashboardPage = async (req: Request, res: Response) => {
+  return res.render('admin/dashboard/show.ejs');
+};
+const getAdminUserPage = async (req: Request, res: Response) => {
+  return res.render('admin/user/show.ejs');
+};
+export { getDashboardPage, getAdminUserPage };
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -7,6 +7,10 @@ import {
   getViewUser,
   postUpdateUser,
 } from 'controllers/user.controller';
+import {
+  getAdminUserPage,
+  getDashboardPage,
+} from 'controllers/admin/dashboard.controller';
 
 const router = express.Router();
 const webRoutes = (app: Express) => {
@@ -21,6 +25,11 @@ const webRoutes = (app: Express) => {
   router.get('/handelViewUser/:id', getViewUser);
 
   router.post('/handel-update-user', postUpdateUser);
+
+  //admin routes
+  router.get('/admin', getDashboardPage);
+
+  router.get('/admin/user', getAdminUserPage);
 
   app.use('/', router);
 };
