0ff968bad20b4ef41ae9ff9f8da550af30367d50
mnhquan <mnhquan0109@gmail.com>
2026-02-18 12:43:13 +0700

    Video 90

--- src/controllers/admin/dashboard.controller.ts
+++ src/controllers/admin/dashboard.controller.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express';
-import { getAllUsers } from 'services/user.service';
+import { getAllRoles, getAllUsers } from 'services/user.service';
 
 const getDashboardPage = async (req: Request, res: Response) => {
   return res.render('admin/dashboard/show.ejs');
 };
 const getAdminUserPage = async (req: Request, res: Response) => {
   const users = await getAllUsers();
+  const roles = await getAllRoles();
   return res.render('admin/user/show.ejs', {
     users: users,
+    roles,
   });
 };
 const getAdminOrderPage = async (req: Request, res: Response) => {
