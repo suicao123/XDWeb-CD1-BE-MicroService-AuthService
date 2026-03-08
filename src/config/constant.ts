7bd3a872a6f38d6f02f4a7955a92dab4dc45f6f0
mnhquan <mnhquan0109@gmail.com>
2026-03-08 10:54:27 +0700

    Update Final

--- src/config/constant.ts
+++
@@ -1,8 +0,0 @@
-export const ACCOUNT_TYPE = {
-  SYSTEM: 'SYSTEM',
-  GOOGLE: '',
-  GITHUB: '',
-};
-export const TOTAL_ITEMS_PER_PAGE = 1;
-export const TOTAL_ITEMS_PRODUCT_PAGE = 5;
-export const TOTAL_ITEMS_ORDER_PAGE = 2;
