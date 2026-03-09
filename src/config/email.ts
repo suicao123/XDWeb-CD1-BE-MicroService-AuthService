a9ecbc2b23ef223b62396e7ec6bc417accc0f878
mnhquan <mnhquan0109@gmail.com>
2026-03-09 13:03:00 +0700

    modify email.ts

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,15 +1,17 @@
 import nodemailer from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 // Tạo transporter
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT),
-  secure: true, // true for 465, false for other ports
+  port: Number(process.env.EMAIL_PORT),
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
-});
+  family: 4,
+} as SMTPTransport.Options);
 
 // Verify connection
 transporter.verify((error, success) => {
