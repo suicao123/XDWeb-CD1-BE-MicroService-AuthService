08532c044fd83387296a5c69898a1a47353287aa
mnhquan <mnhquan0109@gmail.com>
2026-03-09 14:05:09 +0700

    modify email.ts

--- src/config/email.ts
+++ src/config/email.ts
@@ -3,14 +3,17 @@ import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 // Tạo transporter
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: 465,
-  secure: true,
+  host: process.env.EMAIL_HOST || 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  requireTLS: true,
   family: 4,
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
 } as SMTPTransport.Options);
 
 // Verify connection
